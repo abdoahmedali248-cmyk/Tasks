a1630fee7314f9cd0d63f1759474d41498aaa898
abdoahmedali248 <abdoahmedali24@gmail.com>
2026-05-01 20:25:06 +0300

    Task 7

---
+++ DataDownloader/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var result = await DownloadDataAsync();
+        Console.WriteLine(result);
+    }
+
+    static async Task<string> DownloadDataAsync()
+    {
+        await Task.Delay(2000); 
+        return "Data downloaded";
+    }
+}
